2b5e6e3ad0bb07cc5f2205a182957ef8274d7868
harsh <harsha.ranganna@gmail.com>
2018-05-26 09:37:37 +0530

    second

--- testmaven/src/test/java/com/test/maven/testmaven/Tracker.java
+++ testmaven/src/test/java/com/test/maven/testmaven/Tracker.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features={"Features/login.feature"} ,
-                   glue={"runner"}, strict=true)
+                   glue={"runner"}, strict=true , plugin= {"pretty", "html:target/Destination"})
 public class Tracker {
 
 
